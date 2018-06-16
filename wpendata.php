acf0ffa13b2cf5e59ed1158fc39059923cd8062b
Benfarhat <benfarhat.elyes@gmail.com>
2018-06-17 00:55:14 +0100

    Plugin's main file

---
+++ wpendata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Opendata Alternative
+ * Plugin URI: https://github.com/Benfarhat/wpendata
+ * Description: A simple alternative to CKAN and DKAN for Wordpress Lovers
+ * Version: 1.0.0
+ * Author: Benfarhat Elyes
+ * Author URI: https://github.com/Benfarhat
+ * License: GPL2
+ */
